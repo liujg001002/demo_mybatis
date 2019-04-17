6d3c13460dd2db7a5d67f62c4a6f8bbf38f28ef6
liujianguo <liujianguo@cdyhsoft.com>
2019-04-17 14:59:26 +0800

    移除aop多余包

--- src/main/java/com/mybatis/test/DemoMybatisApplication.java
+++ src/main/java/com/mybatis/test/DemoMybatisApplication.java
@@ -3,9 +3,11 @@ package com.mybatis.test;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-@MapperScan("com.mybatis.test.dao")
+@EnableAspectJAutoProxy//开启aop
+@MapperScan("com.mybatis.test.dao")//mybatis扫描包
 public class DemoMybatisApplication {
 
 	public static void main(String[] args) {
